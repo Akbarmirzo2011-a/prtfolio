894b4299417d97f622ec89ce88982fdd2e2c10a2
Akbarmirzo2011-a <bobojonb619@gmail.com>
2025-10-18 14:33:27 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const gameContainer = document.querySelector(".container"),
+userResult = document.querySelector(".user_result img"),
+cpuResult = document.querySelector(".cpu_result img"),
+result = document.querySelector(".result"),
+optionImages = document.querySelectorAll(".option_images");
+
+optionImages.forEach((image,index)=>{
+    image.addEventListener("click",()=>{
+        image.classList.add("active")
+    })
+})
